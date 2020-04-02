a13e25dd29387323e693db3494739ee273c03821
letscodedev <devarshpanchal@rediffmail.com>
2020-04-02 15:57:16 +0530

    Major change

--- static/js/script.js
+++ static/js/script.js
@@ -3,69 +3,29 @@ $(document).ready(function() {
     const appConfig = new blockstack.AppConfig(['store_write', 'publish_data'])
     var userSession = new blockstack.UserSession({ appConfig })
 
+    $('.show').on('click', function() {
+        $('#content').animate({
+            left: 2000
+        }, 2000)
+        $('#content').removeClass('#content');
+        $('#image-blockchain').animate({
+            right: 2000
+        }, 2000)
+        $('#image-blockchain').removeClass('#image-blockchain');
 
-    if ($(window).width() <= 1199) {
-        console.log("Mobile View");
-        $('.show').on('click', function() {
-            $('#content').animate({
-                    left: 400
-            }, 'slow')
-            $('#content').fadeOut('slow');
+        $('.yo').delay("slow").fadeIn();
+        $('.yo').css('display', 'flex');
 
-            //$("#image-blockchain").css('width', '75%');
-            $("#image").animate({
-                'margin': '10%',
-                'margin-top': '50%'
-            }, 'slow');
-
-            setTimeout(function() {
-                $("#testing").fadeIn();
-                var count = 0;
-                do {
-                    $('#testing').fadeOut(1000).fadeIn(1000);
-                    count++;
-                } while (count < 15);
-            }, 2000);
-        });
-    } else {
-        $('.show').on('click', function() {
-            $('#content').animate({
-                left: 800
-            }, 'slow')
-            $('#image-blockchain').animate({
-                'margin-left': '34%',
-                'width': '40%'
-            }, 'slow');
-            //$('#content').fadeOut('slow');
-            // $("#image").animate({
-            //     'width': '50%',
-            //     'margin-left': '28%'
-            // }, 'slow');
-
-            // $('#content').animate({
-            //     left: 1000
-            // }, 'fast').fadeOut('slow');
-
-
-
-
-
-
-            // $("#image").animate({
-
-            // }, 'slow');
-
-            setTimeout(function() {
-                if (!userSession.isUserSignedIn() || !userSession.isSignInPending()) {
-                    userSession.redirectToSignIn()
-                }
-                $("#testing").fadeIn();
-                var count = 0;
-                do {
-                    $('#testing').fadeOut(1000).fadeIn(1000);
-                    count++;
-                } while (count < 15);
-            }, 2000);
-        });
-    }
+        setTimeout(function() {
+            if (!userSession.isUserSignedIn() || !userSession.isSignInPending()) {
+                userSession.redirectToSignIn()
+            }
+            $("#testing").fadeIn();
+            var count = 0;
+            do {
+                $('#testing').fadeOut(1000).fadeIn(1000);
+                count++;
+            } while (count < 15);
+        }, 2000);
+    });
 })
