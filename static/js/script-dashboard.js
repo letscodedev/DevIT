9c681e99270fdcac6db4441ec6c99df7ece66510
dwij patel <saurabh.sachit39@gmail.com>
2020-04-08 17:24:11 +0530

    lol

--- static/js/script-dashboard.js
+++ static/js/script-dashboard.js
@@ -1,65 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    var domainURL = "http://localhost:8080"
+    const appConfig = new blockstack.AppConfig(['store_write', 'publish_data'])
+    var userSession = new blockstack.UserSession({ appConfig })
     activeProject();
-    var date = new Date();
-    var deadline = new Date('6/12/2021');
-    var startDate = new Date('01/22/2020')
-    var diffTime = Math.abs(date - startDate);
-    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-    console.log(yes(diffDays, 100, 1))
-    function yes(val, max, min) { return ((val - min) / (max - min))*100; }
-    // $("#data").append(deadline - date);
-    $('#progress').animate({
-        'width': diffDays
-    }, 'slow')
+    function activeProject() {
+        (async () => {
+
+            $("#data").empty();
+            let projects = await userSession.getFile("created_project.json", { decrypt: true })
+            if (projects) {
+                projects = JSON.parse(projects)
+                let appendContent = ""
+                let i = 0
+                for (let project in projects.my_projects) {
+
+                    let created_date = new Date(projects.my_projects[project].created_at).getDate() + "/" + new Date(projects.my_projects[project].created_at).getMonth() + "/" + new Date(projects.my_projects[project].created_at).getFullYear()
+                    let innerdata = ""
+                    innerdata += '<div class="project-box" id="' + project + '">'
+                    innerdata += '<div class="project-box-header" style="display:flex; justify-content: space-between;">'
+                    innerdata += '<span>project#' + i + '</span>'
+                    innerdata += '<span><i class="far fa-clock" style="margin-right: 0.5rem;"></i>' + created_date + '</span>'
+                    innerdata += '</div >'
+                    innerdata += '<h1>' + project + '</h1>'
+                    innerdata += '</div >'
+                    appendContent += innerdata
+                    i += 1
+                    $(document).on('click', '#' + project, function () {
+                        window.location = "project-dashboard.html?project_name=" + project
+                    })
+                }
+                $('#data').append('' + appendContent)
+            } else {
+                $('#data').append('No Projects!')
+            }
+
+        })()
+    }
 });
 
-function activeProject() {
-    $("#data").empty();
-    console.log("Active Project");
-    var link = 'https://api.myjson.com/bins/126pnc';
-    $.getJSON(link, function (yo) {
-        // YO should be Array of Posts
-        console.log(yo.data[0])
-        var appendContent = "";
-        $.each(yo['data'], function (index, value) {
-            var innerData = '';
-            innerData += '<div class="project-box"><div class="project-box-header" style="display:flex; justify-content: space-between;"><span> Project # ' + parseInt(index+1) + ' </span><span><i class="far fa-clock"></i> ' + value.startDate + ' </span> </div><h1> ' + value.projectTitle + ' </h1><div class="project-box-buttons" style="display:flex; ">';
-            var tags = ''
-            $.each(value.tags, function (index, value) {
-                tags = '';
-                tags += '<div class="button"> ' + value.tagDes + ' </div>'
-            });
-            innerData += tags;
-            var diffDays = checkProgrss(value.startDate);
-            console.log(diffDays);
-            innerData += '</div><div class="project-box-display" style="display: flex; justify-content: space-between;"><div> ' + value.box1 + '</div><div> ' + value.box2 + '</div><div> ' + value.box3 + '</div></div><div style="margin-top: auto;"><div id="progress' + index + '" style="background-color: red; height: 10px; border-radius: 0 10px 10px 10px;width: '+diffDays+'%"></div></div></div>';
-            $('#progress'+parseInt(index+1)).animate({
-                'width': diffDays
-            }, 'slow')
-            appendContent += innerData;
-        });
-        console.log(appendContent);
-        $('#data').append('' + appendContent);
-    });
-}
 
+/*
 function pastProject() {
     $("#data").empty();
     console.log("Past Project");
     $("#data").append("past");
-}   
+}
 
 function checkProgrss(dateStart) {
     var date = new Date();
     // var deadline = new Date('6/12/2021');
     var startDate = new Date(dateStart)
     var diffTime = Math.abs(date - startDate);
-    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     // console.log(yes(diffDays, 100, 1))
-    function yes(val, max, min) { return ((val - min) / (max - min))*100; }
+    function yes(val, max, min) { return ((val - min) / (max - min)) * 100; }
     return diffDays;
     // $("#data").append(deadline - date);
     // $('#progress').animate({
     //     'width': diffDays
     // }, 'slow')
-}
+}*/
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     const appConfig = new blockstack.AppConfig(['store_write', 'publish_data'])
     var userSession = new blockstack.UserSession({ appConfig })
 
-    $('.show').on('click', function() {
+
+    if (userSession.isSignInPending()) {
+        $('#content').animate({
+            left: 2000
+        }, 2000)
+        $('#content').removeClass('#content');
+        $('#image-blockchain').animate({
+            right: 2000
+        }, 2000)
+        $('#image-blockchain').removeClass('#image-blockchain');
+
+        $('.yo').delay("slow").fadeIn();
+        $('.yo').css('display', 'flex');
+        userSession.handlePendingSignIn()
+            .then(userData => {
+
+                window.location = "dashboard.html"
+
+            })
+
+    }
+
+    $('.show').on('click', function () {
         $('#content').animate({
             left: 2000
         }, 2000)
@@ -16,16 +38,14 @@ $(document).ready(function() {
         $('.yo').delay("slow").fadeIn();
         $('.yo').css('display', 'flex');
 
-        setTimeout(function() {
-            if (!userSession.isUserSignedIn() || !userSession.isSignInPending()) {
+        setTimeout(function () {
+            if (!userSession.isUserSignedIn()) {
                 userSession.redirectToSignIn()
+            } else {
+                window.location = "dashboard.html"
             }
-            $("#testing").fadeIn();
-            var count = 0;
-            do {
-                $('#testing').fadeOut(1000).fadeIn(1000);
-                count++;
-            } while (count < 15);
         }, 2000);
     });
+
+
 })
